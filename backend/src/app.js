e35318e8b80931b3761d1478008a692a633d6391
Tu Nombre <bherrerabarajs@gmail.com>
2025-04-07 21:23:30 -0500

    kick-off

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+/*importamos al framework express */
+
+import express from "express";
+import categoriasRoutes from "./routers/categorias.routes.js"
+
+/*Asignamos a app toda funcionalidad para mi server web */
+
+const app = express();
+
+/*setear un puerto ami web server */
+app.set("port",8000);
+
+/*routers */
+
+app.use("/api/categorias",categoriasRoutes)
+
+export default app;
